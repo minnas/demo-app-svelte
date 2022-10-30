1c3eae11084149cde9536d7c4d13c5fe2a5dd251
Minna Salonen <minsalon@Minnas-MBP.home>
2022-10-31 01:01:34 +0200

    theme switch

--- src/components/tools/AwesomeHeader.svelte
+++ src/components/tools/AwesomeHeader.svelte
@@ -2,13 +2,18 @@
   import Fa from 'svelte-fa'
   import { faSunPlantWilt } from '@fortawesome/free-solid-svg-icons'
   import logo from '@Assets/svelte.svg'
+  import ThemeSwitch from '@Tools/ThemeSwitch.svelte'
+
   export let title = 'Small Demo with Vite + Svelte'
 </script>
 
 <div class="some-header">
   <Fa icon={faSunPlantWilt} />
   <h1>{title}</h1>
-  <img class="svelte-logo" src={logo} alt="" />
+  <div class="right-side">
+    <img class="svelte-logo" src={logo} alt="" />
+    <ThemeSwitch />
+  </div>
 </div>
 
 <style>
@@ -31,6 +36,13 @@
   .some-header .svelte-logo {
     height: 2.5rem;
     width: auto;
-    align-self: flex-end;
+    margin: auto;
+  }
+  .right-side {
+    display: flex;
+    flex-direction: row;
+    grid-column-gap: 1rem;
+    justify-content: center;
+    align-items: center;
   }
 </style>
