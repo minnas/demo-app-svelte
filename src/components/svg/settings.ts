8226d1db6689bab7e07bf89ee23ebfbcbcee6786
Minna Salonen <minsalon@Minnas-MBP.home>
2022-11-06 06:25:10 +0200

    animate SVG fill color

--- src/components/svg/settings.ts
+++
@@ -1,4 +0,0 @@
-export const colors = ['red', 'green', 'blue', 'yellow', 'brown', 'violet', 'white']
-export const ignoreTags = ['svg', 'text']
-export const ignoreColors = ['rgb(0, 0, 0)', '#000000']
-export const clearColor = '#ffffff'
---
+++ src/components/svg/utils.ts
@@ -0,0 +1,8 @@
+export const colors = ['red', 'green', 'blue', 'yellow', 'brown', 'violet', 'white']
+export const ignoreTags = ['svg', 'text']
+export const whiteRegex = /(#fff(f+)?)|(rgba?\(255\S?,255\S?,255\S?(\S?,1)?\))$/i
+export const ignoreColorRegex = /(#000(0+)?)|(rgba?\(0\S?,0\S?,0\S?(,\S?)?\))$/i
+
+export const slideUp = (to: number) => ({opacity: [1, 0], marginTop: ['0', `-${to.toString()}px`]})
+export const slideDown = (from: number) => ({opacity: [0, 1], marginTop: [`-${from.toString()}px`, '0px']})
+export const fillColor = (from: string, to:string) => ({opacity: [0, 1], fill: [from, to],})
